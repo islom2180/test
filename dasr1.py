73618ae66ace2d8c070db42bf22fd0bb7d0b17ca
Your Name <you@example.com>
2026-01-04 17:27:47 +0500

    birinchi

--- dasr1.py
+++ dasr1.py
@@ -1,3 +1,12 @@
-for i in range(5):
-    print('qlae')
-print('agi')
+num = [1,2,3,2,1,2,4,32,2,1,2,2,3,4,2,1,12,3,32,2,2,1,2]
+c=num[0]
+d=dict()
+for i in num:
+    if i in d:
+        d[i]+=1
+    else:
+        d[i]=1
+
+e=max(d,key=d.get)
+print('eng kop  qatnashga son ',e)
+print(e,'soni',d[e],'marta qatnashgan')
